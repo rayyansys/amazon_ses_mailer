5b57f907abe55024c5e33f586d3f718917dbaff3
sabarafiq0987 <sabarafiq0987@gmail.com>
2023-01-28 02:29:47 +0500

    removed rubocop changes

--- lib/amazon_ses_mailer/base.rb
+++ lib/amazon_ses_mailer/base.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 module AmazonSesMailer
   class Base
     attr_reader :template_name
 
     class << self
       attr_accessor :delivery_method
-
+      
       def deliveries
         @@_deliveries ||= []
       end
@@ -16,21 +14,19 @@ module AmazonSesMailer
         unless interceptor_class.respond_to?(:delivering_email)
           raise "#{interceptor_class} does not respond to :delivering_email"
         end
-
         @@__interceptors << interceptor_class
       end
 
       protected
-
       # DSL method for setting defaults
-      def default(*args)
+      def default(*args, &block)
         @default_options ||= {}
         @default_options.merge!(args.extract_options!)
       end
     end
 
     def default_options
-      (self.class.instance_variable_get(:@default_options) || {}).merge({ template: @template_name })
+      (self.class.instance_variable_get(:@default_options) || {}).merge({template: @template_name})
     end
 
     def initialize(template_name)
@@ -39,18 +35,16 @@ module AmazonSesMailer
     end
 
     def self.method_missing(method_name, *args, &block)
-      template_name = [name, method_name].join('-')
+      template_name = [self.name, method_name].join('-')
       new(template_name).send(method_name, *args, &block)
     end
 
     def mail(options)
       options = default_options.merge(options)
       options[:merge_vars] = process_merge_vars(options[:merge_vars])
-      if AmazonSesMailer::Base.delivery_method == :test
-        delivery_proc = proc do |delivery|
-          self.class.deliveries << OpenStruct.new(delivery.merge({ template: options[:template] }))
-        end
-      end
+      delivery_proc = Proc.new { |delivery|
+        self.class.deliveries << OpenStruct.new(delivery.merge({template: options[:template]}))
+      } if AmazonSesMailer::Base.delivery_method == :test
       Message.new(options, @@__interceptors, delivery_proc)
     end
 
@@ -58,33 +52,32 @@ module AmazonSesMailer
 
     def process_merge_vars(merge_vars)
       # if no merge_vars specified, extract from instance variables
-      merge_vars ||= convert_instance_variables_to_merge_vars
+      merge_vars = convert_instance_variables_to_merge_vars unless merge_vars
       transform_hash(merge_vars).to_json
     end
 
     def convert_instance_variables_to_merge_vars
-      instance_variable_names.reduce({}) do |result, variable_name|
-        key = variable_name.delete '@'
-        value = instance_variable_get(variable_name)
+      self.instance_variable_names.reduce({}) do |result, variable_name|
+        key = variable_name.delete "@"
+        value = self.instance_variable_get(variable_name)
         result.merge!(key => value)
       end
     end
 
     def transform_hash(hash)
-      hash.transform_values { |value| transform_value(value) }
+      hash.transform_values{ |value| transform_value(value) }
     end
-
+    
     def transform_array(arr)
-      arr.map { |value| transform_value(value) }
+      arr.map{|value| transform_value(value)}
     end
-
+    
     def transform_value(value)
       # recurse on hashes/arrays, converts nil/false values to empty strings, and converts all others to strings
-      return '' if value.nil?
+      return '' unless !!value
       return transform_hash(value) if value.is_a?(Hash)
       return transform_array(value) if value.is_a?(Array)
-
-      value.to_s
+      return value.to_s
     end
 
     def instance_variable_names
@@ -92,3 +85,4 @@ module AmazonSesMailer
     end
   end
 end
+
