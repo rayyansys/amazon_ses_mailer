3db3ec3f358c02ec220dc3c0ad48a1a547a0fcbc
Omar <omar@rayyan.ai>
2025-02-20 17:58:06 +0200

    Remove sanitizer_proc option and auto sanitize

--- lib/amazon_ses_mailer/base.rb
+++ lib/amazon_ses_mailer/base.rb
@@ -77,8 +77,13 @@ module AmazonSesMailer
       return '' unless !!value
       return transform_hash(value) if value.is_a?(Hash)
       return transform_array(value) if value.is_a?(Array)
-      return default_options[:sanitizer_proc].call(value.to_s) if default_options[:sanitizer_proc].present?
-      return value.to_s
+      return sanitize(value.to_s)
+    end
+
+    def sanitize(str)
+      escape_table = { "'" => '&#39;', '&' => '&amp;', '"' => '&quot;', '<' => '&lt;', '>' => '&gt;', }
+
+      str.gsub(/['&\"<>]/, escape_table)
     end
   end
 end
